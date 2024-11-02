20c0561c01c50e53937f59ff2cee89bcea77ab50
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-11-01 17:43:10 -0700

    Implemented button component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // Adjust this based on your file structure
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#507DCD",
+        secondary: "#3360b1",
+      },
+    },
   },
   plugins: [],
 };

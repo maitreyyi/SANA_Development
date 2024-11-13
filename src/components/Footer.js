7287423d2c17f8e462418900c8c1e49d60cd8c01
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2024-11-13 14:55:16 -0800

    feat: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Footer(){
-    return (
-        <footer className="bg-blue-600 text-white p-4 text-left">
-          <p>SANA software</p>
-          <p>Written by Nil Mamano under the supervision of Dr. Wayne B. Hayes at U.C. Irvine.</p>
-          <hr class="border-t border-gray-300 my-4"></hr>
-          <p>SANA Web Interface</p>
-          <p>Current Developer: Maitreyi Sinha</p>
-          <hr class="border-t border-gray-300 my-4"></hr>
-          <p>Please contact the currebt developer above and/or Dr. Wayne B. Hayes for questions, comments, or bugs.</p>
-        </footer>
-    );
+function Footer() {
+  return (
+    <footer className="bg-primary text-base text-white p-4 text-left">
+      <h4 className="font-bold text-lg">SANA software</h4>
+      <p className="py-2">
+        Written by Nil Mamano under the supervision of{" "}
+        <Link
+          href="mailto:whayes@uci.edu"
+          className="underline hover:text-hover-link transition"
+        >
+          Wayne B. Hayes
+        </Link>{" "}
+        at U.C. Irvine.
+      </p>
+      <Link
+        href="https://github.com/waynebhayes/SANA"
+        className="underline hover:text-hover-link transition"
+      >
+        SANA software GitHub
+      </Link>
+
+      <hr class="border-t border-gray-300 my-4"></hr>
+
+      <h4 className="font-bold text-lg">SANA Web Interface</h4>
+      <p className="py-2">Current Developer: Maitreyi Sinha</p>
+      <Link href="TODO" className="underline hover:text-hover-link transition">
+        SANA Web Interface GitHub (TO BE ADDED)
+      </Link>
+
+      <hr class="border-t border-gray-300 my-4"></hr>
+
+      <h4 className="font-bold text-lg">Questions and Contact</h4>
+      <p className="py-2">
+        Contact the current developer above and/or Dr. Wayne B. Hayes for
+        questions, comments, or bugs.
+      </p>
+      <p className="py-2 text-xs">Last update November 13, 2024</p>
+    </footer>
+  );
 }
 
 export default Footer;
-

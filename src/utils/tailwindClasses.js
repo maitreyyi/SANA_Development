c1013e541f9ad9cdbf636320726e0fc1939d8d18
Jose <josegonz115@gmail.com>
2024-11-13 21:02:05 -0800

    feat: contact page + api folder + utils(tailwind) folder

---
+++ src/utils/tailwindClasses.js
@@ -0,0 +1,57 @@
+export const inputClasses = `
+    w-full
+    px-4
+    py-2
+    rounded-full
+    border
+    border-gray-300
+    focus:outline-none
+    focus:border-blue-500
+    focus:ring-2
+    focus:ring-blue-200
+    transition-all
+    duration-200
+`;
+
+export const buttonClasses = `
+    w-full 
+    x-6 
+    py-2 
+    text-white 
+    font-medium 
+    rounded-full 
+    bg-blue-500 
+    hover:bg-blue-600 
+    active:bg-blue-700 
+    focus:outline-none 
+    focus:ring-2 
+    focus:ring-blue-300 
+    transition-all 
+    duration-200 
+    shadow-md
+`;
+
+export const labelClasses = `
+    block 
+    text-sm 
+    font-medium 
+    text-gray-700
+`;
+
+export const headingTwoClasses = `
+    text-3xl 
+    font-bold 
+    text-center 
+    mb-8
+`;
+
+export const containerClasses = `
+    mx-auto
+    bg-slate-200 
+    rounded-xl 
+    shadow-md 
+    p-8 
+    w-full 
+    max-w-[500px] 
+    space-y-6
+`;

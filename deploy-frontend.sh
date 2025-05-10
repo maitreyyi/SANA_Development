cad70fbc9a3418bb4830dc01a318b5b3b1ec0b23
Group account for Hayes <sana@archimedes-11.ics.uci.edu>
2025-05-09 17:57:40 -0700

    bug: deploy-backend.sh doesn't restart tmux server

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -2,10 +2,15 @@
 
 directory="$HOME/app/frontend"
 cd $directory
-exec npm run build
+echo "Building static production files..."
+npm run build
+echo "Copying build files into /var/www/hayeslab/..."
 sudo cp -r dist/* /var/www/hayeslab/
+echo "Adding read permissions..."
+sudo chmod -R 755 /var/www/hayeslab
 sudo ln -s /etc/nginx/sites-available/hayeslab /etc/nginx/sites-enabled/
 sudo nginx -t
+echo "Restarting nginx server..."
 sudo systemctl reload nginx
 sudo systemctl restart nginx
 echo "Nginx status:"
